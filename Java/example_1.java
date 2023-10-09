911a674c9e8825c9c7258e10fe9ff614dcab33ca
unknown <duckcuack920@gmail.com>
2023-10-08 18:09:36 -0600

    Hao git commit -m Hao git status git clear a

---
+++ Java/example_1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class example_1 {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+System.out.println("Selecciona una opción (1 para Saludar, 2 para Despedirse, 3 para Salir): ");
+
+int opcion = scanner.nextInt();
+
+switch (opcion) {
+    case 1:
+        System.out.println("Hola, ¡bienvenido!");
+        break;
+    case 2:
+        System.out.println("Adiós, ¡hasta luego!");
+        break;
+    case 3:
+        System.out.println("Saliendo del programa...");
+        break;
+    default:
+        System.out.println("Opción no válida");
+}
+scanner.close(); }}
